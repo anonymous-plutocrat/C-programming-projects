d596123db994af4f470f95dcae472ff056bdc1f8
anonymous-plutocrat <fcmokwenye@gmail.com>
2019-10-26 20:59:04 +0100

    Update Project 10(ft. AP).c
    I updated a few errors some of which are;
    • Use of the integer identifier %d to identify the float unit_load.
    • The For loop having i=0, and i <=ask. Here you have ask as an input, the user puts 3 but the computer actually reads in four values(i.e from zero to 3). What should've been used is i<ask or i=1. In this update, i used i=1.
    • I also added an if ladder to highlight the range of the final GP being either first class, second class etc.
    •

--- project10.c
+++ project10.c
@@ -13,12 +13,12 @@ int main()
     printf("How many courses are you offering : ");
     scanf("%d", &ask);
 
-    for(i=0; i<=ask ; i++) {
+    for(i=1; i<=ask ; i++) {
 
-        printf("Enter your grade: ");
+        printf("Enter your grade for course %d: ", i);
         scanf("%s", &grade);
         printf("Enter its unitload: ");
-        scanf("%d", &unit_load);
+        scanf("%f", &unit_load);
         if (grade == 'A' || grade == 'a') { multiplier = 5; }
         if (grade == 'B' || grade == 'b') { multiplier = 4; }
         if (grade == 'C' || grade == 'c') { multiplier = 3; }
@@ -32,18 +32,18 @@ int main()
         totalunitload += unit_load;
         gp1 = total2 / totalunitload;
     }
-printf("Your first semester gp is %.3f\n\n", gp1);
+printf("Your first semester gp is %.2f\n\n", gp1);
  printf("FOR SECOND SEMESTER:\n");
 
     printf("How many courses are you offering : ");
     scanf("%d", &ask);
 
-    for(i=0; i<=ask; i++)
+    for(i=1; i<=ask; i++)
     {
-        printf("Enter grade for course %d:\n", ask);
+        printf("Enter grade for course %d: ", i);
         scanf("%s", &grade);
         printf("Enter its unitload\n");
-        scanf("%d", &unit_load);
+        scanf("%f", &unit_load);
         if(grade=='A'||grade=='a'){multiplier=5;}
         if(grade=='B'||grade=='b'){multiplier=4;}
         if(grade=='C'||grade=='c'){multiplier=3;}
@@ -59,7 +59,26 @@ printf("Your first semester gp is %.3f\n\n", gp1);
     printf("Your second semester gp is %.3f\n\n", gp2);
 
     CGPA=(gp1+gp2)/2;
-    printf("YOUR CGPA FOR THE SESSION IS %.3f", CGPA);
+    printf("YOUR CGPA FOR THE SESSION IS %.2f", CGPA);
+    
+    if(CGPA >=4.5 && CGPA <=5.0){
+    printf("\nIt lies within the First Class Range\n");
+    }
 
+    else if(CGPA >=3.5 && CGPA < 4.5){
+    printf("\nIt lies within the Second Class Upper Range\n");
+    }
+
+    else if(CGPA >=2.5 && CGPA < 3.5){
+    printf("\nIt lies within the Second Class lower Range\n");
+    }
+
+    else if(CGPA >=1.5 && CGPA <2.5){
+    printf("\nIt lies within the Third Class Range\n");
+    }
+
+    else{
+    printf("\nIt lies within the Pass Range\n");
+    }
     return 0;
 }
